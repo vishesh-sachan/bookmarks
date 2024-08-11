2153a49a84966555218c8cd97ddbcd3ed9800c9b
vishesh-sachan <visheshsachan021@gmail.com>
2024-08-11 14:40:40 +0530

    added font , Signup ,Signin and landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      hello
+      <div className="flex justify-center font-SFmono bg-[#000000] text-[#FFFFFF] h-10 text-2xl">
+       BookMarks
+      </div>
+      <div className="flex justify-center font-SFmono text-6xl text-[#FFFFFF] mt-230">BookMarks Manager</div>
+      <div className="flex justify-center font-SFmono text-2xl text-[#FFFFFF] mt-10">Keep your Links Handy</div>
+      <div className="flex justify-center font-SFmono text-lg text-[#999999] mt-230">Why Need It? Because Arc Browser Does't Have It!</div>
+      <div className="flex justify-center gap-20 mt-50">
+      <Link href={'/signup'}><div className="bg-[#FFFFFF] flex justify-center px-12 py-2 rounded">SignUp</div></Link>
+      <Link href={'/signin'}><div className="bg-[#FFFFFF] flex justify-center px-12 py-2 rounded">SignIn</div></Link>
+      </div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,28 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
-  },
+    fontFamily:{
+      'SFmono' : ['SF-Mono-Regular' , 'regular']
+    },
+    fontSize: {
+      'sm': '0.8rem',
+      'base': '1rem',
+      'xl': '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+      '6xl': '5.000rem',
+      '7xl': '5.500rem',
+    },
+    margin:{
+      '230':'230px',
+      '100':'100px',
+      '50':'50px',
+      '10':'10px',
+      '15':'15px',
+      '20':'20px',
+    }},
   plugins: [],
 };
 export default config;
