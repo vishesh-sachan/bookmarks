986ad78e0d9fff7f3ec2c73845b988700e419145
vishesh-sachan <visheshsachan021@gmail.com>
2024-08-18 06:47:00 +0530

    added vercel Analytics

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,11 +2,13 @@
 import React from "react";
 import { SessionProvider } from "next-auth/react";
 import { RecoilRoot } from "recoil";
+import { Analytics } from "@vercel/analytics/react"
 
 export function Providers ({children} : { children : React.ReactNode }){
     return(
         <SessionProvider>
             <RecoilRoot>
+                <Analytics />
                 {children}
             </RecoilRoot>
         </SessionProvider>
