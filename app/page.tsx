67c875b6562b83909d6ee8589ecd00d8f83c27fe
vishesh-sachan <visheshsachan021@gmail.com>
2024-11-04 19:59:47 +0530

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <div className="flex justify-center font-SFmono bg-[#000000] text-[#FFFFFF] h-10 text-2xl">BookMarks</div>
       <div className="flex justify-center font-SFmono text-5xl md:text-6xl text-[#FFFFFF] mt-230">BookMarks Manager</div>
       <div className="flex justify-center font-SFmono text-2xl text-[#FFFFFF] mt-10">Keep your Links Handy</div>
-      <div className="flex justify-center font-SFmono text-lg lg:text-xl xl:text-2xl text-[#999999] mt-230">Why Need It? Because Arc Browser Does't Have It!</div>
+      <div className="flex justify-center font-SFmono text-lg lg:text-xl xl:text-2xl text-[#999999] mt-230">Why Need It? Because Why Not!</div>
       <div className="flex justify-center gap-20 mt-50">
       <Link href={'/signup'}><div className="bg-[#FFFFFF] flex justify-center px-12 py-2 rounded">SignUp</div></Link>
       <Link href={'/api/auth/signin'}><div className="bg-[#FFFFFF] flex justify-center px-12 py-2 rounded">SignIn</div></Link>
